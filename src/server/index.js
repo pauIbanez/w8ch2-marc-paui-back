c7391d4831cb9ed6707f0d94cdb9fdaa8b981c86
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-02 18:52:46 +0100

    added router to chain

--- src/server/index.js
+++ src/server/index.js
@@ -3,11 +3,15 @@ const cors = require("cors");
 const morgan = require("morgan");
 const { default: helmet } = require("helmet");
 
+const tweetsRouter = require("./routers/tweetsRouter");
+
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+
+app.use("/tweet", tweetsRouter);
 
 module.exports = app;
