c00bef03549e5041b742c788c80745376c0e88d1
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-02 18:32:41 +0100

    added server to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const connectToDB = require("./database");
+const app = require("./server");
+const startServer = require("./server/startServer");
 
+const port = process.env.PORT || 400;
 const connectionString = process.env.MONGO_DB_STRING;
 
 (async () => {
   await connectToDB(connectionString);
+  await startServer(port, app);
 })();
