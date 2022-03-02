2ee0f643a3920e7184a72f92dd2d4c0d65d1e35e
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-02 19:35:54 +0100

    tweetsController valid request test created

--- src/server/controllers/tweetsController.js
+++ src/server/controllers/tweetsController.js
@@ -3,17 +3,11 @@ const Tweet = require("../../database/models/Tweet");
 const deleteTweet = async (req, res, next) => {
   const { id } = req.params;
   try {
-    const tweet = Tweet.findById(id);
-    if (!tweet) {
-      const error = new Error("Tweet not found");
-      error.status = 404;
-      return next(error);
-    }
-    await Tweet.deleteOne({ _id: id });
-    return res.status(200).json({});
+    await Tweet.findByIdAndDelete(id);
+    res.json({});
   } catch (error) {
     error.message = "Error trying to delete the tweet";
-    return next(error);
+    next(error);
   }
 };
 
---
+++ src/server/controllers/tweetsController.test.js
@@ -0,0 +1,21 @@
+const Tweet = require("../../database/models/Tweet");
+const { deleteTweet } = require("./tweetsController");
+
+describe("Given a deleteTweet controller", () => {
+  describe("When it receives a request with a valid id", () => {
+    test("Then it should return an empty object", async () => {
+      const tweet = { date: "", text: "hello", likes: 2, id: 3 };
+      const req = { params: { id: tweet.id } };
+
+      Tweet.findByIdAndDelete = jest.fn().mockResolvedValue({});
+
+      const res = { json: jest.fn() };
+
+      await deleteTweet(req, res);
+
+      expect(res.json).toHaveBeenCalled();
+      // Act
+      // Assert
+    });
+  });
+});
