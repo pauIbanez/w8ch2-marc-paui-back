9bba9ffb87590b88faf43d7e1ef15f0b608612de
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-02 19:42:10 +0100

    tweetsController invalid request tested

--- src/server/controllers/tweetsController.test.js
+++ src/server/controllers/tweetsController.test.js
@@ -14,8 +14,19 @@ describe("Given a deleteTweet controller", () => {
       await deleteTweet(req, res);
 
       expect(res.json).toHaveBeenCalled();
-      // Act
-      // Assert
+    });
+  });
+  describe("When it receives a request without a valid id", () => {
+    test("Then it should return an error", async () => {
+      const tweet = { date: "", text: "hello", likes: 2, id: 3 };
+      const req = { params: { id: tweet.id } };
+
+      Tweet.findByIdAndDelete = jest.fn().mockRejectedValue({});
+
+      const next = jest.fn();
+      await deleteTweet(req, null, next);
+
+      expect(next).toHaveBeenCalled();
     });
   });
 });
